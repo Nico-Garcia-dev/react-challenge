82184db27304c9fc2e624218891b2416f59d4b64
NICO GARCIA <07nico.garcia@gmail.com>
2025-04-04 17:22:45 +0200

    filter ok

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,21 +2,13 @@ import type { CardProps } from "../types/types";
 
 import "../style/card.css";
 
-export default function Card({
-	title,
-	description,
-	price,
-	image,
-	category,
-}: CardProps) {
+export default function Card({ title, description, price, image }: CardProps) {
 	return (
 		<section className="card">
 			<img src={image} alt="article pic" />
 			<h2>{title}</h2>
 			<span>Prix: {price}€</span>
 			<p>{description}</p>
-
-			<h3>{category}</h3>
 		</section>
 	);
 }
--- src/components/cardList.tsx
+++ src/components/cardList.tsx
@@ -1,23 +1,55 @@
-import data from "../data/data.json";
+import { useState } from "react";
 import type { ArticleType } from "../types/types.ts";
 
+import data from "../data/data.json";
 import "../style/cardList.css";
 
 import Card from "./card";
 
 export default function CardList() {
+	// putting items category on an array to handle filter buttons creation
+	const categArray: string[] = [
+		...new Set(data.map((item) => item.category)),
+		"all",
+	];
+	//console.log(categArray);
+
+	//Filtering logic
+	// using useState to get the filtered category: initialized for all objects categories
+	const [filtered, setFiltered] = useState("all");
+	//Using handleclick for the event triggering
+	function handleClickCategory(e) {
+		setFiltered(e.target.textContent);
+
+		console.log(filtered);
+	}
+	// displaying filtered objects using filter()
+	const filteredData = data.filter(
+		(item) => item.category === filtered || "all" === filtered,
+	);
+
 	return (
 		<main>
-			{data.map((d: ArticleType) => (
-				<Card
-					key={d.id}
-					title={d.title}
-					description={d.description}
-					price={d.price}
-					image={d.image}
-					category={d.category}
-				/>
-			))}
+			<div className="filterBtn">
+				{categArray.map((c) => (
+					<button type="button" key={`k.${c}`} onClick={handleClickCategory}>
+						{c}
+					</button>
+				))}
+			</div>
+
+			<div className="cards">
+				{filteredData.map((f: ArticleType) => (
+					<Card
+						key={f.id}
+						title={f.title}
+						description={f.description}
+						price={f.price}
+						image={f.image}
+						category={f.category}
+					/>
+				))}
+			</div>
 		</main>
 	);
 }
